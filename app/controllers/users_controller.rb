f9c7d0ab29546450365d394affa648b32eaefeec
saracsilva <saracarolinasilva@gmail.com>
2023-06-19 23:13:46 +0200

    created login config and sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,19 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def show
+    
+  end
   def new
     @user = User.new
   end
 
   def edit
-    @user = User.find(params[:id])
+   
   end
 
   def update
-    @user = User.find(params[:id])
+   
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
       redirect_to articles_path
@@ -22,6 +27,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to alpha blog #{@user.username}"
       redirect_to articles_path
     else
@@ -34,4 +40,8 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
